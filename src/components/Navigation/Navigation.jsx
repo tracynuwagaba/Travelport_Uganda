3a6c10daa2bac81f3acd5bc70d1355074b008260
TracyNuwagaba <tnuwagaba191@gmail.com>
2021-06-08 15:43:06 +0300

    create navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function Navigation() {
+  return (
+    <>
+      <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/services">Services</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Navigation;
